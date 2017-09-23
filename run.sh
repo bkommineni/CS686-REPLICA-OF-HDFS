8dfa437a151214d4422eb835244735a10b01aca1
Bhargavi Kommineni <bkommineni@stargate.cs.usfca.edu>
2017-09-23 13:24:37 -0700

    script to kill all servers running on all nodes

--- run.sh
+++ run.sh
@@ -1,104 +1,26 @@
 #!/usr/bin/env bash
 
+node_list=(bass02 bass03 bass04 bass05 bass06 bass07 bass08 bass09 bass10 bass11 bass12 bass13 bass14 bass15 bass16 bass17 bass18 bass19 bass20)
+
 #Controller (bass01)
 ssh bass01 "
 cd ~/Documents/courses/cs686/p1-bkommineni;
 /usr/local/maven/bin/mvn compile package;
 cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.Controller 9999 &
+java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.Controller 9999 > controller.out 2>&1 &
 "
 
 #StorageNodes
-ssh bass02 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass03 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass04 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass05 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass06 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass07 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass08 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass09 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass10 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass11 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass12 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass13 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass14 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass15 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass16 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass17 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass18 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass19 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass20 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
+
+for node in ${node_list[@]};do
+	ssh $node "
+	cd ~/Documents/courses/cs686/p1-bkommineni/target/;
+	java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 > storage.out 2>&1 &
+	"
+done
+
+#Client
 ssh bass21 "
 cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass22 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-ssh bass23 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 &
-"
-#Client
-ssh bass24 "
-cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.Client bass01 9999 store clientDirectory/File1.txt &
+java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.Client bass01 9999 store clientDirectory/File1.txt > client.out 2>&1 &
 "

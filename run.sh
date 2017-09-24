e866a99464b408cf45c00674d9657ac0ffc808ba
Bhargavi Kommineni <bkommineni@stargate.cs.usfca.edu>
2017-09-24 08:12:36 -0700

    modifications to script file to make development easier

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-node_list=(bass02 bass03 bass04 bass05 bass06 bass07 bass08 bass09 bass10 bass11 bass12 bass13 bass14 bass15 bass16 bass17 bass18 bass19 bass20)
+node_list=(bass02 bass04 bass07 bass08 bass09 bass11)
 
 #Controller (bass01)
 ssh bass01 "
 cd ~/Documents/courses/cs686/p1-bkommineni;
 /usr/local/maven/bin/mvn compile package;
 cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.Controller 9999 > controller.out 2>&1 &
+java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.Controller 9000 > controller.out 2>&1 &
 "
 
 #StorageNodes
@@ -15,12 +15,13 @@ java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.Controller 9999 > co
 for node in ${node_list[@]};do
 	ssh $node "
 	cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-	java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9999 > storage.out 2>&1 &
+	rm storage_${node}.out;
+	java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.StorageNode bass01 9000 > storage_${node}.out 2>&1 &
 	"
 done
 
 #Client
-ssh bass21 "
+ssh bass12 "
 cd ~/Documents/courses/cs686/p1-bkommineni/target/;
-java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.Client bass01 9999 store ~/Documents/courses/cs686/p1-bkommineni/clientDirectory/File1.txt > client.out 2>&1 &
+java -cp dfs-1.0-jar-with-dependencies.jar edu.usfca.cs.dfs.Client bass01 9000 store ~/Documents/courses/cs686/p1-bkommineni/clientDirectory/File1.txt > client.out 2>&1 &
 "
